06c523056b9260a00572e2b7ae17e6bb167b1e1a
Gunaseelan <gunaseelan1226@gmail.com>
2026-06-15 18:35:15 +0530

    Add longestConsecutive function to find the length of the longest consecutive sequence

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -15,3 +15,23 @@ var moveZeroes = function(nums) {
     return nums;
 };
 console.log(moveZeroes([0,1,0,3,12])); // [1,3,12,0,0]
+
+// 02.Longest Consecutive Sequence
+
+var longestConsecutive = function(nums) {
+    const set = new Set(nums);
+    let longest = 0;
+    for(let num of set){
+        if(!set.has(num-1)){
+            let current = num;
+            let length =1;
+            while(set.has(current+1)){
+                current++;
+                length++;
+            }
+        longest = Math.max(longest,length);
+        }
+    }
+        return longest;  
+};
+console.log(longestConsecutive([100,4,200,1,3,2])); // 4
